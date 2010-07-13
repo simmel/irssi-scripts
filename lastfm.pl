f9ba0b59306e2cf58ffb1a05259fa2562e9a2a3e
Simon Lundström <simon@soy.se>
2010-07-13 17:06:43 +0200

    5.5 release * Rewrote the whole error handling * Fixed a bug where the error messages would be said and not printed. * Fixed some minor bugs and removed some unneeded code. * Stole the whole forky-ness implementation from dns.pl which people said worked, because the implementation that I hacked did not work sometimes.

--- lastfm.pl
+++ lastfm.pl
@@ -1,8 +1,8 @@
-# vim: set noexpandtab:
+# vim: set expandtab:
 use vars qw($VERSION %IRSSI);
-$VERSION = "5.4";
+$VERSION = "5.5";
 %IRSSI = (
-        authors     => "Simon 'simmel' Lundström",
+        authors     => "Simon 'simmel' LundstrÃ¶m",
         contact     => 'simmel@(freenode|quakenet|efnet) http://last.fm/user/darksoy',
         name        => "lastfm",
         date        => "20100526",
@@ -60,6 +60,11 @@ Irssi::settings_add_bool("lastfm", "lastfm_use_action", 0);
 Irssi::settings_add_bool("lastfm", "lastfm_get_player", 0);
 
 # Changelog#{{{
+
+# 5.5 -- Mon Jul 12 19:04:26 CEST 2010
+# * Rewrote the whole error handling
+# * Fixed a bug where the error messages would be said and not printed.
+# * Fixed some minor bugs and removed some unneeded code.
 
 # 5.4 -- Wed May 26 17:04:08 CEST 2010
 # * Last.fm updated their profile HTML so that the %player macro didn't work.
@@ -219,186 +224,197 @@ Irssi::settings_add_bool("lastfm", "lastfm_get_player", 0);
 # Move along now, there's nothing here to see.
 
 sub DEBUG {
-	Irssi::settings_add_bool("lastfm", "lastfm_debug", 0);
-	Irssi::settings_get_bool("lastfm_debug");
+  Irssi::settings_add_bool("lastfm", "lastfm_debug", 0);
+  Irssi::settings_get_bool("lastfm_debug");
 };
 
 use strict;
-no strict 'refs';
 use warnings;
-use LWP::UserAgent;
+use Data::Dumper;
+use Encode;
 use HTML::Entities;
 use Irssi;
-use Encode;
-use Data::Dumper;
-use POSIX '_exit';
+use LWP::UserAgent;
+use POSIX;
 
-my $errormsg_pre = "You haven't submitted a song to Last.fm";
-my $errormsg_post = ", maybe Last.fm submission service is down?";
-our ($pid, $input_tag, %cache) = undef;
-our $api_key = "eba9632ddc908a8fd7ad1200d771beb7";
-our $fields = "(artist|name|album|url|player)";
-our $ua = LWP::UserAgent->new(agent => "lastfm.pl/$VERSION", timeout => 10);
+my $pipe_tag;
+my $api_key = "eba9632ddc908a8fd7ad1200d771beb7";
+my $fields = "(artist|name|album|url|player)";
+my $ua = LWP::UserAgent->new(agent => "lastfm.pl/$VERSION", timeout => 10);
 
-sub cmd_lastfm {
-	my ($data, $server, $witem) = @_;
-	lastfm_forky($witem, $data);
+sub lastfm_nowplaying {
+  my ($content, $url, $response, $tag, $value, %data);
+  my ($user_shifted, $is_tabbed, $nowplaying, $witem) = @_;
+  my $user = $user_shifted || Irssi::settings_get_str("lastfm_user");
+  $nowplaying ||= ((Irssi::settings_get_str("lastfm_output_tab_complete") ne "" && $is_tabbed) ? Irssi::settings_get_str("lastfm_output_tab_complete") : Irssi::settings_get_str("lastfm_output"));
+
+  my $command_message = ($is_tabbed) ? '%%np(username)' : '/np username';
+  if ($user eq '') {
+    return "ERROR: You must /set lastfm_user to a username on Last.fm or use $command_message";
+  }
+
+  if ($nowplaying =~ /%(lastfm|lfm)/) {
+    return "http://last.fm/user/$user/";
+  }
+
+  $url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$user&api_key=$api_key&limit=1";
+  print Dumper "Checking for scrobbles at: $url" if DEBUG;
+  $response = $ua->get($url);
+  $content = $response->content;
+
+  # TODO This should work, untested (fail more Last.fm! ; )
+  if ($content =~ m!<lfm status="failed">.*<error .*?>([^<]+)!s) {
+    return "ERROR: $1";
+  }
+  my @data = split('\n', $content);
+
+  if (!grep(m!<track nowplaying="true">!, @data)) {
+    print Dumper \$response if DEBUG;
+    return "ERROR: You are not playing anything according to Last.fm. Check http://www.last.fm/user/$user and see if they turn up there, otherwise restart your scrobbler.";
+  }
+
+  my $regex = qr!<$fields.*?(?:uts="(.*?)">.*?|>(.*?))</\1>!;
+
+  foreach my $data (@data) {
+    if ($data =~ m!</track>!) {
+      last;
+    }
+    elsif ($data =~ /$regex/) {
+      ($tag, $value) = ($1, (defined($2) ? $2 : $3));
+      print Dumper \$tag, \$value, \$data if DEBUG;
+      $data{$tag} = $value;
+    }
+  }
+
+  if (Irssi::settings_get_bool("lastfm_get_player")) {
+    $url = "http://www.last.fm/user/$user";
+    $content = $ua->get($url)->content;
+    if ($content =~ m!<span class="source">(.*?)</span>!) {
+      $_ = $1;
+      s/<[^>]*>//mgs;
+      $data{'player'} = $_;
+    }
+    else {
+      print "Couldn't find the player even though lastfm_get_player was set" if DEBUG;
+    }
+  }
+
+  print Dumper \%data if DEBUG;
+  print Dumper "Output pattern before: $nowplaying" if DEBUG;
+  $nowplaying =~ s/(%\((.*?%(\w+).?)\))/($data{$3} ? $2 : "")/ge;
+  print Dumper "Output pattern after: $nowplaying" if DEBUG;
+  $nowplaying =~ s/%$fields/$data{$1}/ge;
+  decode_entities($nowplaying);
+  Encode::from_to($nowplaying, "utf-8", Irssi::settings_get_str("term_charset"));
+  $nowplaying = "$user_shifted $nowplaying" if $user_shifted;
+  return $nowplaying;
 }
 
-sub lastfm {
-		my ($content, $url, $response, $tag, $value, %data); 
-		my $user_shifted = shift;
-		my $user = $user_shifted || Irssi::settings_get_str("lastfm_user");
-		my $is_tabbed = shift;
-		my $nowplaying = shift || ((Irssi::settings_get_str("lastfm_output_tab_complete") ne "" && $is_tabbed) ? Irssi::settings_get_str("lastfm_output_tab_complete") : Irssi::settings_get_str("lastfm_output"));
-
-		my $command_message = ($is_tabbed) ? '$np(username)' : '/np username';
-		die("You must /set lastfm_user to a username on Last.fm or use $command_message\n") if $user eq '';
-
-		$url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$user&api_key=$api_key&limit=1";
-		print Dumper "Checking for scrobbles at: $url" if DEBUG;
-		$response = $ua->get($url);
-		$content = $response->content;
-
-		# TODO This should work, untested (fail more Last.fm! ; )
-		die $1."\n" if ($content =~ m!<lfm status="failed">.*<error .*?>([^<]+)!s);
-		my @data = split('\n', $content);
-
-		if (!grep(m!<track nowplaying="true">!, @data)) {
-			print Dumper \$response if DEBUG;
-			die "You are not playing anothing according to Last.fm. Check http://www.last.fm/user/$user and see if they turn up there, otherwise restart your scrobbler.\n";
-		}
-
-		my $regex = qr!<$fields.*?(?:uts="(.*?)">.*?|>(.*?))</\1>!;
-
-		foreach my $data (@data) {
-			if ($data =~ m!</track>!) {
-				last;
-			}
-			elsif ($data =~ /$regex/) {
-				($tag, $value) = ($1, (defined($2) ? $2 : $3));
-				print Dumper \$tag, \$value, \$data if DEBUG;
-				$data{$tag} = $value;
-			}
-		}
-
-		if (Irssi::settings_get_bool("lastfm_get_player")) {
-			$url = "http://www.last.fm/user/$user";
-			$content = $ua->get($url)->content;
-			if ($content =~ m!<span class="source">(.*?)</span>!) {
-				$_ = $1;
-				s/<[^>]*>//mgs;
-				$data{'player'} = $_;
-			}
-			else {
-				print "Couldn't find the player even though lastfm_get_player was set" if DEBUG;
-			}
-		}
-
-		print Dumper \%data if DEBUG;
-		print Dumper "Output pattern before: $nowplaying" if DEBUG;
-		$nowplaying =~ s/(%\((.*?%(\w+).?)\))/($data{$3} ? $2 : "")/ge;
-		print Dumper "Output pattern after: $nowplaying" if DEBUG;
-		$nowplaying =~ s/%$fields/$data{$1}/ge;
-		decode_entities($nowplaying);
-		Encode::from_to($nowplaying, "utf-8", Irssi::settings_get_str("term_charset"));
-		$nowplaying = "$user_shifted $nowplaying" if $user_shifted;
-		return $nowplaying;
+sub lastfm_blocking {
+  my ($witem, $user) = @_;
+  my $nowplaying = lastfm_nowplaying($user, undef, undef, $witem);
+  lastfm_print($witem, $nowplaying);
 }
 
 sub lastfm_forky {
-	my $witem = shift;
-	my $user = shift || 0;
-	my @user = split(/ /, $user);
-	$user = $user[0];
-	if ($pid or $input_tag) {
-		Irssi::active_win()->print("We're still waiting for Last.fm to return our data or to hit the timeout (this happends when Last.fm is down or very slow).");
-		return;
-	}
+  my ($witem, $user) = @_;
+  # pipe is used to get the reply from child
+  my ($rh, $wh);
+  pipe($rh, $wh);
 
-	my ($reader, $writer);
-	pipe($reader, $writer);
-	$pid = fork();
-	if (!defined $pid) {
-		close $reader;
-		close $writer;
-		return;
-	}
-	if ($pid > 0) {
-		close $writer;
-		Irssi::pidwait_add($pid);
-		my @args = ($witem, $reader);
-		$input_tag = Irssi::input_add(fileno($reader), INPUT_READ, \&input_read, \@args);
-		return;
-	}
-	my $response;
-	eval {
-		$response = lastfm($user);
-	};
-	if ($@) {
-		$response = $@;
-	}
-	print $writer $response;
-	close $writer;
-	POSIX::_exit(1);
+  # non-blocking host lookups with fork()ing
+  my $pid = fork();
+  if (!defined($pid)) {
+    Irssi::print("Can't fork() - aborting");
+    close($rh);
+    close($wh);
+    return;
+  }
+
+  if ($pid > 0) {
+    # parent, wait for reply
+    close($wh);
+    Irssi::pidwait_add($pid);
+    $pipe_tag = Irssi::input_add(fileno($rh), INPUT_READ, \&pipe_input, [$witem, $rh]);
+    return;
+  }
+
+  my $text;
+  eval {
+    # child, do the lookup
+    $text = lastfm_nowplaying($user);
+  };
+
+  if (!$text) {
+    $text = "ERROR: Error message: $!";
+  }
+
+  eval {
+    # write the reply
+    print($wh $text);
+    close($wh);
+  };
+  POSIX::_exit(1);
 }
 
-sub input_read {
-	my ($witem, $reader) = @{$_[0]};
-	my @content = <$reader>;
-	my $content = join('', @content);
 
-	if ($content =~ /^(.+) at (.*?\/lastfm.pl|\(eval \d+\)) line \d+\.$/) {
-		my $message = $1;
-		Irssi::active_win()->print($message);
-	}
-	else {
-		if (defined $witem->{type} && $witem->{type} =~ /^QUERY|CHANNEL$/) {
-			if (Irssi::settings_get_bool("lastfm_use_action")) {
-				$witem->command("me $content");
-			}
-			else {
-				$witem->command("say $content");
-			}
-		}
-		else {
-			print($content);
-		}
-	}
+sub pipe_input {
+  my ($witem, $rh) = @{$_[0]};
+  my $text = <$rh>;
+  close($rh);
 
-	Irssi::input_remove($input_tag);
-	close $reader;
-	$input_tag = $pid = undef;
+  Irssi::input_remove($pipe_tag);
+  $pipe_tag = -1;
+
+  lastfm_print($witem, $text);
 }
 
-Irssi::command_bind('np', 'cmd_lastfm', 'lastfm');
+sub lastfm_print {
+  my ($witem, $text) = @_;
+  # Fugly error handling
+  if ($text =~ s/^ERROR: //) {
+    Irssi::active_win()->print($text);
+    return;
+  }
+
+  if (defined $witem->{type} && $witem->{type} =~ /^QUERY|CHANNEL$/) {
+    if (Irssi::settings_get_bool("lastfm_use_action")) {
+      $witem->command("me $text");
+    }
+    else {
+      $witem->command("say $text");
+    }
+  }
+  else {
+    Irssi::active_win()->print($text);
+  }
+}
+
+Irssi::command_bind('np', sub {
+    my ($data, $server, $witem) = @_;
+    $data =~ s/ .*//;
+    $data ||= 0;
+    if (DEBUG) {
+      lastfm_blocking($witem, $data);
+    }
+    else {
+      lastfm_forky($witem, $data);
+    }
+  }, 'lastfm');
 
 Irssi::signal_add_last 'complete word' => sub {
-	my ($complist, $window, $word, $linestart, $want_space) = @_;
-	my $is_tabbed = 1;
-	my $tab_fields = $fields;
-	$tab_fields =~ s/\(/(nowplaying|np|/;
-	if ($word =~ /%(lastfm|lfm)/) {
-		my $user = Irssi::settings_get_str("lastfm_user");
-		if (!defined $user) {
-			Irssi::active_win()->print("You must /set lastfm_user to a username on Last.fm");
-			return;
-		}
-		push @$complist, "http://last.fm/user/$user/";
-	}
-	elsif ($word =~ /(\%(?:$tab_fields))\(?(\w+)?\)?/) {
-		my ($nowplaying, $user) = ($1, $3);
-		undef $nowplaying if ($nowplaying =~ /nowplaying|np/);
-		eval {
-			$nowplaying = lastfm($user, $is_tabbed, $nowplaying);
-		};
-		if ($@) {
-			if ($@ =~ /^(.+) (.*?\/lastfm.pl|\(eval \d+\)) line \d+\.$/) {
-				my $message = $1;
-				Irssi::active_win()->print($message);
-			}
-			return 0;
-		}
-		push @$complist, "$nowplaying" if $nowplaying;
-	}
+my ($complist, $window, $word, $linestart, $want_space) = @_;
+my $is_tabbed = 1;
+my $tab_fields = $fields;
+$tab_fields =~ s/\(/(nowplaying|np|lastfm|lfm|/;
+if ($word =~ /(\%(?:$tab_fields))\(?(\w+)?\)?/) {
+my ($nowplaying, $user) = ($1, $3);
+undef $nowplaying if ($nowplaying =~ /nowplaying|np/);
+$nowplaying = lastfm_nowplaying($user, $is_tabbed, $nowplaying);
+if ($nowplaying =~ s/^ERROR: //) {
+Irssi::active_win()->print($nowplaying);
+return 0;
+}
+push @$complist, "$nowplaying" if $nowplaying;
+}
 };
